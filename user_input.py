a4000bb83c7d23507810518b8ecfad0a0264aa78
Moseskip254 <160002211+Moseskip254@users.noreply.github.com>
2024-03-04 11:35:15 +0300

    initial commit

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Prompting the user for input
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you from? ")
+
+# Printing out a personalized message
+print(f"Hello {name}! You are {age} years old and you are from {location}.")
